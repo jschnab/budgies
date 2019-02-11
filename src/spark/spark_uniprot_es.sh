fc1a2ffa0f267f05834e826177e2b0a8e8aba639
Jonathan Schnabel <jonathan.schnabel31@gmail.com>
2019-02-11 17:03:30 -0500

    merge scripts loadin uniprot data to elasticsearch

--- src/spark/spark_uniprot_es.sh
+++
@@ -1,5 +0,0 @@
-# Spark job which parses Uniprot files and place data in Elasticsearch
-# (accession, {pdb, ensembl, refseq)
-# index: uniprot
-
-$SPARK_HOME/bin/spark-submit --jars ~/elasticsearch-hadoop-6.5.4/dist/elasticsearch-hadoop-6.5.4.jar --master spark://ec2-3-92-97-223.compute-1.amazonaws.com:7077 spark-trial.py
